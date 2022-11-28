2cc8f3524f095600fab9689d3b6576256e4d0ed8
apjanco <apjanco@gmail.com>
2022-11-28 15:55:41 -0500

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,12 @@
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+const yaml = require("js-yaml");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(EleventyI18nPlugin, {
+      // any valid BCP 47-compatible language tag is supported
+      defaultLanguage: "en", // Required, this site uses "en"
+      errorMode: "never"
+    });
+    eleventyConfig.addPassthroughCopy("assets");
+    eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
+  };
